7ee36a4fd11aeb060115093db676c9908b896229
mo7g <mr.mohammed1913@gmail.com>
2024-11-09 13:58:11 +0200

    added cv button

--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import {AiFillGithub,AiFillLinkedin} from 'react-icons/ai'
 import {HiOutlineMail} from 'react-icons/hi'
+import { PiReadCvLogoBold } from "react-icons/pi";
+
 
 const SocialMedia = () => (
   <div className="app__social">
+    <a href="https://docs.google.com/document/d/1Nwx2N3sssqoIQrYpYX0VDksXdjmIfYzvCg1Mn5yhPl4/edit?tab=t.0">
+    <div>
+      <PiReadCvLogoBold />
+    </div>
+    </a>
     <a href="https://github.com/MO7G" target="_blank" rel="noreferrer">
       <div>
       <AiFillGithub />
