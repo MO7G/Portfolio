c415b9918ac797c85b89826b58731733c4e0d832
mo7g <mr.mohammed1913@gmail.com>
2025-07-24 18:45:35 +0300

    done

--- client/src/components/SocialMedia.jsx
+++ client/src/components/SocialMedia.jsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import {AiFillGithub,AiFillLinkedin} from 'react-icons/ai'
-import {HiOutlineMail} from 'react-icons/hi'
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { HiOutlineMail } from "react-icons/hi";
 import { PiReadCvLogoBold } from "react-icons/pi";
-
 
 const SocialMedia = () => (
   <div className="app__social">
-    <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1Nwx2N3sssqoIQrYpYX0VDksXdjmIfYzvCg1Mn5yhPl4/edit?tab=t.0">
-    <div>
-      <PiReadCvLogoBold />
-    </div>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://docs.google.com/document/d/1Nwx2N3sssqoIQrYpYX0VDksXdjmIfYzvCg1Mn5yhPl4/edit?tab=t.0"
+    >
+      <div>
+        <PiReadCvLogoBold />
+        <div></div>
+      </div>
     </a>
     <a href="https://github.com/MO7G" target="_blank" rel="noreferrer">
       <div>
-      <AiFillGithub />
+        <AiFillGithub />
       </div>
     </a>
 
-    <a href="https://www.linkedin.com/in/muhamed-elhag/" target="_blank" rel="noreferrer">
-    <div>
-      <AiFillLinkedin />
-    </div>
+    <a
+      href="https://www.linkedin.com/in/muhamed-elhag/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div>
+        <AiFillLinkedin />
+      </div>
     </a>
 
     <a href="mailto:muhamedelhagg@gmail.com">
-    <div>
-      <HiOutlineMail />
-    </div>
+      <div>
+        <HiOutlineMail />
+      </div>
     </a>
   </div>
 );
